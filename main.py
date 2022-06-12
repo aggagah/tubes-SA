4399302616126e69cbcb04e7f1ea96760bb0ce12
aggagah <gagahsp4@gmail.com>
2022-06-12 08:03:15 +0700

    initialize main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+    TUGAS BESAR STRATEGI ALGORITMA
+    KELOMPOK 14 - IF4401
+    ANGGOTA:
+        Herman Gemilang - 1301204014
+        Gagah Aji Gunadi - 1301204093
+        Syahdan Naufal Nur Ihsan - 1301204110
+    PROBLEM : 
+    SOLUTION ALGORITHM: 
+"""
